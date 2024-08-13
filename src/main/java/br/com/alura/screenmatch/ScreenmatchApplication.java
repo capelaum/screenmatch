8608e2cbd3ae91f572662f0133c81057e987943c
capelaum <thecapellett@gmail.com>
2024-08-13 19:51:21 -0300

    feature: consume API Service

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatch.service.ConsumeApiService;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
@@ -13,7 +15,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primeiro projeto Spring sem Web");
+		ConsumeApiService consumeApiService = new ConsumeApiService();
+		String json = consumeApiService.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+		json = consumeApiService.getData("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
 	}
 
 }
